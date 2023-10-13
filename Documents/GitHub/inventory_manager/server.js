e961050aa829c3d861e4572690b7355398575f65
Zain Ali <zain1pakistan9988@gmail.com>
2023-10-13 02:19:22 -0400

    Added the main look for the frontend

--- Documents/GitHub/inventory_manager/server.js
+++ Documents/GitHub/inventory_manager/server.js
@@ -3,7 +3,14 @@ const app = express();
 
 const port = 3000;
 
+
 app.set('view engine', 'ejs');
 app.use(express.static('Public'));
 
-app.listen(port);
+app.listen(port, function(error) {
+    if(error) {
+        console.log("Soimething went wrong", error);
+    } else {
+        console.log("Server listening on port " + port);
+    }
+});
